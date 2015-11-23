d261c0d1f0f3537c70e27b1c1ad454bd0de4fb9a
Karuna <imkaruna@gmail.com>
2015-11-23 14:39:29 +0000

    nitrous-backup at Mon Nov 23 14:39:29 UTC 2015

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,18 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+  factor_list = Array.new
+  i=2
+  j=2
+  if input > 1
+    while i <= input
+      if input%i ==0
+          sqrt(i)
+          factor_list << i         
+      end
+     i+=1
+    end 
+  end
+  print factor_list
+  k=2 
+  prime_factor_list = factor_list.reject{|j| j%k==0 }
+end
+
